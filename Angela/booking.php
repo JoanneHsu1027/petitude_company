1f76646da2a0bcbbeeae7867290a06b64ca85a1f
Angela <angelakao519@gmail.com>
2024-05-15 11:37:41 +0800

    [success]已完成CRUD功能

--- Angela/booking.php
+++ Angela/booking.php
@@ -24,11 +24,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-    <?php include __DIR__ . '/parts/html_head.php' ?>
+     <?php include __DIR__ . '/parts/html_head.php' ?>
         <?php include __DIR__ . '/parts/navbar.php' ?>
     <div class="container">
         <div class="row">
-            <a href="booking-admin.php">list</a>
+            <div class="col-6">
+            <div class="card">
+
+                <div class="card-body">
+                <h5 class="card-title">登入</h5>
+
+                <form name="form1" onsubmit="sendData(event)">
+                    <div class="mb-3">
+                    <label for="email" class="form-label">電子郵箱</label>
+                    <input type="text" class="form-control" id="email" name="email">
+                    <div class="form-text"></div>
+                    </div>
+                    <div class="mb-3">
+                    <label for="password" class="form-label">密碼</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                    <div class="form-text"></div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">登入</button>
+                </form>
+
+                </div>
+            </div>
+            </div>
         </div>
     </div>
 
@@ -86,7 +109,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isPass) {
         const fd = new FormData(document.form1); // 沒有外觀的表單物件
 
-        fetch(`Angela/backend/login-api.php`, {
+        fetch(`login-api.php`, {
             method: 'POST',
             body: fd,
         }).then(r => r.json()).then(data => {
--- Angela/project-no-admin.php
+++ Angela/project-no-admin.php
@@ -1,149 +1,136 @@
-<!-- 沒有權限 -->
-<?php
-require __DIR__ . '/config/pdo_connect.php';
-$title = '生前契約列表-無權限';
-$pageName = 'project-no-admin';
+    <?php
+    require __DIR__ . '/config/pdo_connect.php';
+    $title = '生前契約列表-無權限';
+    $pageName = 'project-no-admin';
 
-$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+    $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
-if ($page < 1) {
-header('Location: ?page=1');
-exit;
-}
-
-$perPage = 25; # 每頁有幾筆
-# 算總筆數 $totalRows
-$t_sql = "SELECT COUNT(1) FROM project";
-$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
-$totalPages = ceil($totalRows / $perPage); # 總頁數
-
-$rows = []; # 預設值
-# 如果有資料的話
-if ($totalRows) {
-if ($page > $totalPages) {
-    header('Location: ?page=' . $totalPages);
+    if ($page < 1) {
+    header('Location: ?page=1');
     exit;
-}
+    }
 
-$sql = sprintf("SELECT * FROM `project` ORDER BY project_id ASC LIMIT %s, %s", ($page - 1) * $perPage, $perPage);
-$rows = $pdo->query($sql)->fetchAll();
-}
+    $perPage = 25; # 每頁有幾筆
+    # 算總筆數 $totalRows
+    $t_sql = "SELECT COUNT(1) FROM project";
+    $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+    $totalPages = ceil($totalRows / $perPage); # 總頁數
 
-?>
-<?php include __DIR__ . '/parts/html_head.php' ?>
-<?php include __DIR__ . '/parts/navbar.php' ?>
+    $rows = []; # 預設值
+    # 如果有資料的話
+    if ($totalRows) {
+    if ($page > $totalPages) {
+        header('Location: ?page=' . $totalPages);
+        exit;
+    }
 
-<div id="content">
-    <h1>生前契約列表-無權限</h1>
-</div>
-<div class="container">
-<div class="row">
-    <div class="col">
-    <nav aria-label="Page navigation example">
-        <ul class="pagination">
-        <li class="page-item">
-            <a class="page-link" href="#">
-            <i class="fa-solid fa-angles-left"></i>
-            </a>
-        </li>
+     $sql = sprintf("SELECT * FROM `project` ORDER BY project_id ASC LIMIT %s, %s", ($page - 1) * $perPage, $perPage);
+    $rows = $pdo->query($sql)->fetchAll();
+    }
 
-        <li class="page-item">
-            <a class="page-link" href="#">
-            <i class="fa-solid fa-angle-left"></i>
-            </a>
-        </li>
+    ?>
+    <?php include __DIR__ . '/parts/html_head.php' ?>
+    <?php include __DIR__ . '/parts/navbar.php' ?>
 
-        <?php for ($i = $page - 5; $i <= $page + 5; $i++) : ?>
-            <?php if ($i >= 1 and $i <= $totalPages) : ?>
-            <li class="page-item <?= $page == $i ? 'active' : '' ?>">
-                <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
-            </li>
-            <?php endif ?>
-        <?php endfor ?>
-        <li class="page-item">
-            <a class="page-link" href="#">
-            <i class="fa-solid fa-angle-right"></i>
-            </a>
-        </li>
-        <li class="page-item">
-            <a class="page-link" href="#">
-            <i class="fa-solid fa-angles-right"></i>
-            </a>
-        </li>
-        </ul>
-    </nav>
+    <!-- page頁籤 -->
+    <div id="content">
+        <h1>生前契約列表-無權限</h1>
     </div>
-</div>
-<div class="row">
-    <div class="col">
-    <form name="form1" onsubmit="sendMultiDel(event)">
-    <table class="table table-bordered table-striped">
-        <thead>
-        <tr>
-            <th class="hidden-column"><input type="checkbox" name="select-all" id="select-all" onclick="toggleAllCheckboxes()"></th>
+    <div class="container">
+    <div class="row">
+        <div class="col">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+            <li class="page-item">
+                <a class="page-link" href="#">
+                <i class="fa-solid fa-angles-left"></i>
+                </a>
+            </li>
+
+            <li class="page-item">
+                <a class="page-link" href="#">
+                <i class="fa-solid fa-angle-left"></i>
+                </a>
+            </li>
+
+            <?php for ($i = $page - 5; $i <= $page + 5; $i++) : ?>
+                <?php if ($i >= 1 and $i <= $totalPages) : ?>
+                <li class="page-item <?= $page == $i ? 'active' : '' ?>">
+                    <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                </li>
+                <?php endif ?>
+            <?php endfor ?>
+            <li class="page-item">
+                <a class="page-link" href="#">
+                <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">
+                <i class="fa-solid fa-angles-right"></i>
+                </a>
+            </li>
+            </ul>
+        </nav>
+        </div>
+    </div>
+            <!-- 新增/搜尋 -->
+            <!-- <form class="d-flex">
+                <a href="backend/add-project.php" class="btn btn-secondary fa-solid fa-circle-plus m-3"></a>
+                <input class="form-control w-25 mt-2 me-2" style="height: 50px;" type="search" placeholder="Search" aria-label="Search">
+                <a href=""><button class="btn btn-success mt-2" style="height: 50px;" type="submit">查詢</button></a>
+            </form> -->
+            <!-- 新增/搜尋 -->
+        <div class="row">
+        <div class="col">
+        <form id="form1" name="form1" onsubmit="sendMultiDel(event)">
+        <table class="table table-bordered table-striped">
+            <thead>
+            <tr>
                 <th>project_id</th>
                 <th>project_level</th>
                 <th>project_name</th>
                 <th>project_content</th>
                 <th>project_fee</th>
-            <th class="hidden-column">編輯</th>
-            <th class="hidden-column">刪除</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($rows as $r) : ?>
-            <tr>
-            <td class="hidden-column"><input type="checkbox" name="project[]" value="<?= $r['project_id'] ?>"></td>
+                <!-- <th>編輯</th>
+                <th>刪除</th> -->
+            </tr>
+            </thead>
+            <!-- table欄位 -->
+            <!-- 欄位值 -->
+            <tbody>
+            <?php foreach ($rows as $r) : ?>
+                <tr>
                 <td><?= $r['project_id'] ?></td>
                 <td><?= $r['project_level'] ?></td>
                 <td><?= $r['project_name'] ?></td>
                 <td><?= $r['project_content'] ?></td>
                 <td><?= $r['project_fee'] ?></td>
-            <td class="hidden-column">
-                <?php if (isset($_SESSION['admin'])) : ?>
-                <a href="/parts/edit-project.php?project_id=<?= $r['project_id'] ?>">
-                <i class="fa-solid fa-pen-to-square btn btn-warning"></i>
+                <!-- <td>
+                <a href="./backend/edit-project.php?project_id=<?=$r['project_id'] ?>">
+                    <i class="fa-solid fa-pen-to-square btn btn-warning"></i>
                 </a>
-            <?php endif; ?>
-            </td>
-            <td class="hidden-column">
-                <?php if (isset($_SESSION['admin'])) : ?>
-                <a href="javascript: delete_one(<?= $r['project_id'] ?>)">
-                <i class="fa-solid fa-trash-can btn btn-danger"></i>
-                </a>
-            <?php endif; ?>
-            </td>
-            </tr>
-        <?php endforeach ?>
-        </tbody>
-    </table>
+                </td>
+                <td>
+                    <a href="javascript: deleteOne(<?= $r['project_id'] ?>)">
+                    <i class="fa-solid fa-trash-can btn btn-danger"></i>
+                    </a>
+                </td> -->
+                </tr>
+            <?php endforeach ?>
+            </tbody>
+        </table>
+        </form>
+        </div>
     </div>
-</div>
-</div>
-<?php include __DIR__ . '/parts/script.php' ?>
-<script>
-function delete_one(project_id) {
-    if (confirm(`是否要刪除編號為 ${project_id} 的資料?`)) {
-        location.href = `delete-project.php?project_id=${project_id}`;
+    </div>
+    <?php include __DIR__ . '/parts/script.php' ?>
+    <!-- <script>
+    const deleteOne = (project_id) =>{
+        if(confirm(`是否要刪除編號為 ${project_id} 的資料?`)){
+        location.href = `backend/delete-project.php?project_id=${project_id}`;
+        }
     }
-};
 
-function sendMultiDel(event) {
-event.preventDefault();
-const fd = new FormData(document.getElementById('form1'));
-fetch('/../multi-delete-project.php', {
-    method: 'POST',
-    body: fd
-}).then(r => r.json()).then(result => {}).catch(ex => console.log(ex));
-}
-
-function toggleAllCheckboxes() {
-    const checkboxes = document.querySelectorAll('input[name="project[]"]');
-    checkboxes.forEach(checkbox => {
-        checkbox.checked = !checkbox.checked;
-    });
-}
-
-</script>
-
-<?php include __DIR__ . '/parts/html_foot.php' ?>
+    </script> -->
+    <?php include __DIR__ . '/parts/html_foot.php' ?>
